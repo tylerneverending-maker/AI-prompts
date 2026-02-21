5e26a02b1eac420098a701da99270d91b58ec564
Tyler Neverending <tylerneverending@gmail.com>
2026-02-21 18:18:41 +0000

    t

--- main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PromptSuggestions from './PromptSuggestions';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <PromptSuggestions />
-  </React.StrictMode>
-);
