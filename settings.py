e0f31203fa8264974c4052b2522d216d7170cc8d
Tyler Neverending <tylerneverending@gmail.com>
2026-02-21 18:36:06 +0000

    new

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-change-me-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
